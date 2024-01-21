2d991a68cb8dda41d1d45238526c0bcd05475b59
Rajib Chandra Das <prorajib6@gmail.com>
2024-01-21 23:10:34 +0600

    add Loader and Message component

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,7 +1,8 @@
 import { Row, Col } from 'react-bootstrap';
 import React from 'react';
-
+import Loader from '../components/Loader.jsx';
 import Product from '../components/Product.jsx';
+import Message from '../components/Message.jsx';
 import { useGetProductsQuery } from '../slices/productApiSlice.js';
 const HomePage = () => {
   const { data: products, isLoading, error } = useGetProductsQuery();
@@ -9,9 +10,11 @@ const HomePage = () => {
   return (
     <>
       {isLoading ? (
-        <h2>Loading.....</h2>
+        <Loader />
       ) : error ? (
-        <div>{error?.data?.message || error.error}</div>
+        <Message variant='danger'>
+          {error?.data?.message || error.error}
+        </Message>
       ) : (
         <>
           <h1>Latest Products</h1>
--- client/src/pages/SingleProductPage.jsx
+++ client/src/pages/SingleProductPage.jsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../components/Rating';
 import { useGetProductDetailsQuery } from '../slices/productApiSlice';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
 const SingleProductPage = () => {
   const { id: productId } = useParams();
   const {
@@ -16,9 +18,11 @@ const SingleProductPage = () => {
       <Link className='btn btn-light my-3'>Go Back</Link>
 
       {isLoading ? (
-        <h2>Loading....</h2>
+        <Loader />
       ) : error ? (
-        <div>{error?.data?.message || error.error}</div>
+        <Message variant='danger'>
+          {error?.data?.message || error.error}
+        </Message>
       ) : (
         <>
           <Row>
