b240a82a0820d739c7739f0a8c73931873a2d804
Rajib Chandra Das <prorajib6@gmail.com>
2024-01-27 21:39:18 +0600

    user profile endpoints

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -71,15 +71,6 @@ const registerUser = asyncHandler(async (req, res) => {
 //@desc Get user Profile
 //@route GET /api/users/profile
 //@access Private
-
-const updateUserProfile = asyncHandler(async (req, res) => {
-  res.send(' update User Profile');
-});
-
-//@desc Update user Profile
-//@route  PUT  /api/users/profile
-//@access Private
-
 const getUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id);
   if (user) {
@@ -88,6 +79,32 @@ const getUserProfile = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not Found!!');
+  }
+});
+
+//@desc Update user Profile
+//@route  PUT  /api/users/profile
+//@access Private
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+    const updatedUser = await user.save();
+
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
     });
   } else {
     res.status(404);
