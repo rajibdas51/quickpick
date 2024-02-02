2179c6974ce66e921bc57fd5d85dea53abb459f4
Rajib Chandra Das <prorajib6@gmail.com>
2024-02-02 17:43:11 +0600

    Paypal api endpoint

--- server/server.js
+++ server/server.js
@@ -30,6 +30,11 @@ app.use('/api/users', userRoutes);
 // order routes
 app.use('/api/orders', orderRoutes);
 
+// Paypal api endpoint
+app.get('/api/config/paypal', (req, res) =>
+  res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
+);
+
 app.use(notFound);
 app.use(errorHandler);
 app.listen(port, () => {
