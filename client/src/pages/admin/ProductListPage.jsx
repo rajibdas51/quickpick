000dc7fd43420ba4dece506bc7e3586595a4656f
Rajib Chandra Das <prorajib6@gmail.com>
2024-02-03 17:16:48 +0600

    show products list for admin

--- client/src/pages/admin/ProductListPage.jsx
+++ client/src/pages/admin/ProductListPage.jsx
@@ -10,8 +10,67 @@ import { toast } from 'react-toastify';
 const ProductListPage = () => {
   const { data: products, isLoading, error } = useGetProductsQuery();
 
-  console.log(products);
-  return <></>;
+  const deleteHandler = (id) => {
+    console.log('delete' + id);
+  };
+
+  return (
+    <>
+      <Row className='align-items-center'>
+        <Col>
+          <h1>Products</h1>
+        </Col>
+        <Col className='text-end'>
+          <Button className='btn-sm m-3'>
+            <FaEdit /> Create Product
+          </Button>
+        </Col>
+      </Row>
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error.message}</Message>
+      ) : (
+        <>
+          <Table striped hover responsive className='table-sm'>
+            <thead>
+              <th>ID</th>
+              <th>NAME</th>
+              <th>PRICE</th>
+              <th>CATEGORY</th>
+              <th>BRAND</th>
+              <th></th>
+            </thead>
+            <tbody>
+              {products.map((product) => (
+                <tr key={product._id}>
+                  <td>{product._id}</td>
+                  <td>{product.name}</td>
+                  <td>{product.price}</td>
+                  <td>{product.category}</td>
+                  <td>{product.brand}</td>
+                  <td>
+                    <LinkContainer to={`/admin/product/${product._id}/edit`}>
+                      <Button variant='light' className='btn-sm mx-2'>
+                        <FaEdit />
+                      </Button>
+                    </LinkContainer>
+                    <Button
+                      variant='danger'
+                      className='btn-sm'
+                      onClick={() => deleteHandler(product._id)}
+                    >
+                      <FaTrash />
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </>
+      )}
+    </>
+  );
 };
 
 export default ProductListPage;
