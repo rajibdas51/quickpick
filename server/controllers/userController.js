e757de0fa98cb8d07434811a98bb61d35f8da981
Rajib Chandra Das <prorajib6@gmail.com>
2024-02-04 21:09:14 +0600

    add getUserById controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -122,6 +122,21 @@ const getUsers = asyncHandler(async (req, res) => {
   res.status(200).json(users);
 });
 
+//@desc Get user by id
+//@route GET /api/users/:id
+//@access Private/Admin
+
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select('-password');
+
+  if (user) {
+    res.status(200).json(user);
+  } else {
+    res.status(404);
+    throw new Error('User not Found!!');
+  }
+});
+
 export {
   authUser,
   registerUser,
